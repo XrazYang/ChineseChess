ea2d4ad8c311827f975ab91ae85c801025379fa0
DLUTYJH <1161051479@qq.com>
2018-03-19 21:15:30 +0800

    init Chinese chess project.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include<QApplication>
-#include"board.h"
+#include <QApplication>
+#include <QLabel>
+#include <QDialog>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc,argv) ;
-
-    Board w ;
-
-    w.show();
-
+    QApplication a(argc, argv);
+    QDialog q;
+    QLabel label(&q);
+    label.setText("hello word !!!");
+    q.show();
     return a.exec();
 }
